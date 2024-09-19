eee52ece62abbbc200991861b9b3e0e72f03a31f
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-09-19 17:51:49 +0200

    send

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react-swc"; // this on might be "import react 
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: "/",
+    base: "/portfolio",
   };
 
   if (command !== "serve") {
