3dccb87df71ece673cd6862da848255e906e39ec
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-09-19 18:34:37 +0200

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,9 @@ import { useState, useEffect } from "react";
 export default function AboutItem() {
   const images = [
     "./img/miget.JPG",
-    "/img/migto.JPG",
-    "/img/migtrer.JPG",
-    "/img/migfire.JPG",
+    "./img/migto.JPG",
+    "./img/migtrer.JPG",
+    "./img/migfire.JPG",
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
