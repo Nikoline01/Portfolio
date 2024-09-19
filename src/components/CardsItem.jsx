0c4bfa0493a0ecf39aff576a5267ad108a227c40
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-09-19 17:49:35 +0200

    nyt

--- src/components/CardsItem.jsx
+++ src/components/CardsItem.jsx
@@ -4,7 +4,11 @@ export default function CardsItem() {
   return (
     <li className="cards-item">
       <Link to="/Project" className="cards-item-link">
-        <img src="/img/HOF.png" alt="STORYTELLING"></img>
+        <img
+          src={`${import.meta.env.BASE_URL}img/HOF.png`}
+          alt="Holm Outdoor Fitness"
+        />
+
         <p>HOLM OUTDOOR FITNESS</p>
       </Link>
       <Link to="/Project" className="cards-item-link">
